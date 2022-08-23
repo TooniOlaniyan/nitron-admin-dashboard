f4746544f00eb81b679b1e2c95ecf3e8a70a2e93
TooniOlaniyan <asalamlatif@gmail.com>
2022-08-23 22:53:38 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css'
 import Home from './pages/home/Home';
 import {BrowserRouter as Router , Route , Routes} from 'react-router-dom'
+import LogIn from './pages/login/LogIn';
+import List from './pages/list/List';
+import Single from './pages/single/Single';
+import New from './pages/new/New';
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path='/' element={<Home/>}/>
+          <Route path='/' >
+            <Route index element={<Home/>}/>
+            <Route path='login' element={<LogIn/>}/>
+            <Route path='users'>
+              <Route index element={<List/>}/>
+              <Route path=':userId' element={<Single/>}/>
+              <Route path='new' element={<New/>}/>
+            </Route>
+          </Route>
         </Routes>
       </Router>
      
