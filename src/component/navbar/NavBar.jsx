639b04ebef0ec3f708fe78a0f7eb290008be0b99
TooniOlaniyan <asalamlatif@gmail.com>
2022-08-24 18:20:02 +0100

    navbar

--- src/component/navbar/NavBar.jsx
+++ src/component/navbar/NavBar.jsx
@@ -3,7 +3,26 @@ import './navbar.scss'
 
 function NavBar() {
   return (
-    <div className='navBar'>NavBar</div>
+    <div className='navBar'>
+      <div className="search">
+        <input type="search" name="search" id="search" />
+      </div>
+      <div className="navLinks">
+        <div className='english'>
+          icon
+          <p>English</p>
+        </div>
+        <div>icon</div>
+        <div>icon</div>
+        <div>icon</div>
+        <div>icon</div>
+        <div>icon</div>
+        <div className='image'>
+          <img src="" alt="" />
+        </div>
+
+      </div>
+    </div>
   )
 }
 
