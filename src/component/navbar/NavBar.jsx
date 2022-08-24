0377255d72ac3b201c0fbb3ce94590ec65145403
TooniOlaniyan <asalamlatif@gmail.com>
2022-08-24 18:32:54 +0100

    navbar

--- src/component/navbar/NavBar.jsx
+++ src/component/navbar/NavBar.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import './navbar.scss'
+import {TiWorld} from 'react-icons/ti'
+import{CgMoon} from 'react-icons/cg'
+import {
+  BiBell,
+  BiListUl,
+  BiMessageRoundedDots,
+  BiExitFullscreen,
+} from 'react-icons/bi'
 
 function NavBar() {
   return (
@@ -9,14 +17,14 @@ function NavBar() {
       </div>
       <div className="navLinks">
         <div className='english'>
-          icon
+          <TiWorld className='icons'/>
           <p>English</p>
         </div>
-        <div>icon</div>
-        <div>icon</div>
-        <div>icon</div>
-        <div>icon</div>
-        <div>icon</div>
+        <div><CgMoon className='icons'/></div>
+        <div><BiExitFullscreen className='icons'/></div>
+        <div><BiBell className='icons'/></div>
+        <div><BiMessageRoundedDots className='icons'/></div>
+        <div><BiListUl className='icons'/></div>
         <div className='image'>
           <img src="" alt="" />
         </div>
