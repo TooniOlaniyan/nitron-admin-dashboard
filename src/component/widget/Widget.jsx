47bbfec5806ef29c4d631785c03712c6312cf7ce
TooniOlaniyan <asalamlatif@gmail.com>
2022-08-26 15:17:11 +0100

    case

--- src/component/widget/Widget.jsx
+++ src/component/widget/Widget.jsx
@@ -1,22 +1,71 @@
 import React from 'react'
 import './widget.scss'
-import {FaArrowUp , FaUserAlt} from 'react-icons/fa'
+import {FaArrowUp , FaUserAlt , FaShoppingCart , FaDollarSign , FaAcquisitionsIncorporated} from 'react-icons/fa'
 
-function Widget({name , valueDone , percentage}) {
+
+function Widget({type , percentage}) {
+  let data;
+  const amount = 100
+  const diff = 20
+  switch (type) {
+    case 'user':
+      data= {
+        title: 'USERS',
+        isMoney: false,
+        link: 'see all users',
+        icon: <FaUserAlt className='icon'/>
+
+      }
+      
+      break;
+    case 'order':
+      data= {
+        title: 'ORDERS',
+        isMoney: false,
+        link: 'View all Orders',
+        icon: <FaShoppingCart className='icon'/>
+
+      }
+      
+      break;
+    case 'earning':
+      data= {
+        title: 'EARNING',
+        isMoney: true,
+        link: 'View Net Earnings',
+        icon: <FaDollarSign className='icon'/>
+
+      }
+      
+      break;
+    case 'balance':
+      data= {
+        title: 'BALANCE',
+        isMoney: true,
+        link: 'View net Balance',
+        icon: <FaAcquisitionsIncorporated className='icon'/>
+
+      }
+      
+      break;
+  
+    default:
+      break;
+  }
   return (
     <div className='widget'>
       <div className='left'>
-        <span className='user'>{name}</span>
-        <span className='counter'>{valueDone}</span>
-        <span className='link'>See all users</span>
+        <span className='user'>{data.title}</span>
+        <span className='counter'>{data.isMoney && '$'} {amount}</span>
+        <span className='link'>{data.link}</span>
       </div>
 
       <div className='right'>
-        <div className={percentage <= '0' ? 'percentage negative' : 'percentage positive'}>
+        <div className='percentage negative'>
           <FaArrowUp />
-          {percentage}
+          {diff}% 
         </div>
-        <FaUserAlt className='icon' />
+        {data.icon}
       </div>
     </div>
   )
