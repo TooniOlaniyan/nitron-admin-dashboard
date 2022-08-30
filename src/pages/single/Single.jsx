4c35319431ee412ff78c467ca2b5849fa796348f
TooniOlaniyan <asalamlatif@gmail.com>
2022-08-30 20:10:34 +0100

    single user page

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import './single.scss'
+import SideBar from '../../component/sidebar/SideBar'
+import NavBar from '../../component/navbar/NavBar'
 
 function Single() {
   return (
-    <div>Single</div>
+    <div className='single'>
+      <SideBar/>
+      <div className="container">
+        <NavBar/>
+        content
+      </div>
+
+    </div>
   )
 }
 
